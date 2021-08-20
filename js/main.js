bc6c3eb85d7514afda3d472fc80248efb65b6ad1
Gustavo Oliveira Viana <gustaolive1234@gmail.com>
2021-08-20 19:37:39 -0300

    Curso de javaScript

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*var nome = "Gustavo Viana";
+var idade = 20;
+var idade2 = 10;
+var frase = "Japao é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+
+var lista = ["pera", "maça", "uva"];
+//lista.push("ovo");
+//lista.pop("maça");
+console.log(lista.toString())
+console.log (lista.join(" - "));*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"},
+{nome:"uva",cor:"roxo"} ];
+
+console.log(frutas[1].nome);
+*/
+
+
+/*
+var idade = prompt("Qual sua idade");
+if(idade <= 18){
+    alert("maior de idade");
+}else {
+    alert("menor de idade")
+}
+*/
+
+
+
+/*
+var count = 0;
+
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+}
+*/
+
+
+/*
+var count
+
+for(count = 0; count<=5;count++){
+    alert(count)
+}
+*/
+
+var d = new Date();
+
+alert(d.getDay());
